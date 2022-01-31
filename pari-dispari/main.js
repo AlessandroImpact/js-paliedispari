ce87a58caadd2e3bfe6ea042742993f7e8de4940
Alessandro Terracciano <terracciano41@gmail.com>
2022-01-31 11:26:30 +0100

    sol alternativa

--- pari-dispari/main.js
+++ pari-dispari/main.js
@@ -3,7 +3,7 @@ L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). 
 Sommiamo i due numeri 
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) 
-Dichiariamo chi ha vinto.*/
+Dichiariamo chi ha vinto.
 
 //FUNZIONI
 
@@ -56,7 +56,70 @@ let solution = winner(userNum, pcNum);
 
 //stampa a video della soluzione
 
-alert(' la somma è : ' + solution);
+alert(' ha vinto chi ha scelto ' + solution);
+
+*/
+
+
+
+
+
+
+
+//SOLUZIONE ALTERNATIVA
+
+
+function getRandomNumber(min, max) {
+
+    return Math.floor(Math.random() * (max - min +1)) + min;
+
+}
+
+
+function checkPariDispari (numero) {
+
+let ritorno;
+
+if(numero%2==0){
+    ritorno = 'PARI';
+}else {
+    ritorno= 'DISPARI';
+}
+
+return ritorno;
+
+}
+
+
+
+let pariDispari = prompt('Scegli pari o dispari').toUpperCase();
+
+if (pariDispari == 'PARI' || pariDispari == 'DISPARI') {
+
+    let numeroUtente = parseInt(prompt('Inserisci un numero tra 1 e 5'));
+
+    if(numeroUtente >= 1 && numeroUtente<=5 || !isNaN(numeroUtente)){
+
+        let numeroComputer = getRandomNumber(1,5);
+        let somma = numeroComputer + numeroUtente;
+        let verificaSomma = checkPariDispari(somma);
+
+        if(verificaSomma == pariDispari){
+            alert('HAI VINTO');
+        }else {
+            alert('I COMPUTER VINCONO ANCORA');
+        }
+
+
+    }else {
+        alert('Hai inserito un valore non valido');
+    }
+}else {
+    alert('hai inserito un valore non valido');
+}
+
+
+
 
 
     
